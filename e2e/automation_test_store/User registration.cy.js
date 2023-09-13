8f9146605615cbbff0369d73ee1ba46ce0b586a4
legolas_him94 <legolas_him94@yahoo.com>
2023-09-13 13:46:32 +0300

    added fixture file

--- e2e/automation_test_store/User registration.cy.js
+++ e2e/automation_test_store/User registration.cy.js
@@ -1,3 +1,5 @@
+/// <reference types="Cypress" />
+
 describe('User registration', () => {
   beforeEach(() => {
     // Visit the registration page
@@ -7,46 +9,25 @@ describe('User registration', () => {
   });
 
   it('Should successfully register a new user with valid information', () => {
-    // Define variables for user information
-    const firstName = 'admin2';
-    const lastName = 'admin2';
-    const email = 'admin7@gmail.com';
-    const region = 'Bristol';
-    const zipCode = '123456';
-    const loginName = 'admin7';
-    const password = 'admin2';
-    const address = 'admin2';
-    const city = 'admin2';
+    cy.fixture('registration-data').then((userData) => {
+      const user1 = userData.user1;
 
-    //Firstname
-    cy.get('#AccountFrm_firstname').type(firstName);
-    //Last name
-    cy.get('#AccountFrm_lastname').type(lastName);
-    //Email
-    cy.get('#AccountFrm_email').type(email);
-    //Address
-    cy.get('#AccountFrm_address_1').type(address);
-    //City
-    cy.get('#AccountFrm_city').type(city);
-    //Region
-    cy.get('#AccountFrm_zone_id').select(region);
-    //Zip code
-    cy.get('#AccountFrm_postcode').type(zipCode);
+      cy.get('#AccountFrm_firstname').type(user1.firstName);
+      cy.get('#AccountFrm_lastname').type(user1.lastName);
+      cy.get('#AccountFrm_email').type(user1.email);
+      cy.get('#AccountFrm_address_1').type(user1.address);
+      cy.get('#AccountFrm_city').type(user1.city);
+      cy.get('#AccountFrm_zone_id').select(user1.region);
+      cy.get('#AccountFrm_postcode').type(user1.zipCode);
+      cy.get('#AccountFrm_loginname').type(user1.loginName);
+      cy.get('#AccountFrm_password').type(user1.password);
+      cy.get('#AccountFrm_confirm').type(user1.password);
+      cy.get('#AccountFrm_agree').check();
+      cy.get('.col-md-2 > .btn').click();
 
-    //Login name
-    cy.get('#AccountFrm_loginname').type(loginName);
-    //Password
-    cy.get('#AccountFrm_password').type(password);
-    //Confirm passwrod
-    cy.get('#AccountFrm_confirm').type(password);
-
-    //Tick privacy box
-    cy.get('#AccountFrm_agree').check();
-    //Click continue button
-    cy.get('.col-md-2 > .btn').click();
-
-    //Assert the registration was succesfull
-    cy.contains(`Welcome back ${firstName}`).should('be.visible');
-    cy.url().should('include', '/success');
+      //Assert the registration was succesfull
+      cy.contains(`Welcome back ${user1.firstName}`).should('be.visible');
+      cy.url().should('include', '/success');
+    });
   });
 });
